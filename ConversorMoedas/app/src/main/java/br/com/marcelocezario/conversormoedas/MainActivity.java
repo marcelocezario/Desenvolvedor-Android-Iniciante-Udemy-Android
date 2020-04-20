d3110894c5919a14f9c62545bc759141e3111c02
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-19 23:04:56 -0300

    Adicionando OnClick

--- ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
+++ ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
@@ -3,11 +3,12 @@ package br.com.marcelocezario.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -20,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDolar = findViewById(R.id.text_Dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_Euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_Calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+        this.mViewHolder.textDolar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_Calculate) {
+            // Lógica
+        }
     }
 
     private static class ViewHolder {
