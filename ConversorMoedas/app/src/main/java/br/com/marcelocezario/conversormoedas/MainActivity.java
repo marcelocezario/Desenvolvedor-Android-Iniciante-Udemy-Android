e0d23df19938959ac46c76d87e8dfabb5508ac8d
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-19 22:55:56 -0300

    Criando ViewHolder para mapear elementos

--- ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
+++ ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
@@ -3,17 +3,29 @@ package br.com.marcelocezario.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText valor = findViewById(R.id.edit_Valor);
-        valor.getText();
+        this.mViewHolder.editValue = findViewById(R.id.edit_Value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_Dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_Euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_Calculate);
+    }
 
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
