4f316d03ca12ca81a078a0c38af00b364b6130a0
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-19 22:34:33 -0300

    Criando id para os elementos e criando variável

--- ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
+++ ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.marcelocezario.conversormoedas;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.editValor);
+        valor.getText();
+
     }
 }
