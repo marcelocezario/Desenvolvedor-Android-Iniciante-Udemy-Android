3256bca37e0b159a413adbe5fdbf515b7edcfcc0
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-19 22:22:17 -0300

    Usando Values Strings e criando linear layout

--- ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
+++ ConversorMoedas/app/src/main/java/br/com/marcelocezario/conversormoedas/MainActivity.java
@@ -1,7 +1,6 @@
 package br.com.marcelocezario.conversormoedas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
