d270def9985231fb0f19504953f0736bd10cb4fe
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-21 21:02:11 -0300

    Salvando informação de confirmação usuário

--- FestaFimDeAno/app/src/main/java/br/com/marcelocezario/festafimdeano/view/DetailsActivity.java
+++ FestaFimDeAno/app/src/main/java/br/com/marcelocezario/festafimdeano/view/DetailsActivity.java
@@ -3,20 +3,40 @@ package br.com.marcelocezario.festafimdeano.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import br.com.marcelocezario.festafimdeano.R;
+import br.com.marcelocezario.festafimdeano.constant.FimDeAnoConstants;
+import br.com.marcelocezario.festafimdeano.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences = new SecurityPreferences();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
+
         this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_participate) {
+            if (this.mViewHolder.checkParticipate.isChecked()){
+                // Salvar presença
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_YES);
+            } else {
+                // Salvar ausência
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_NO);
+            }
+        }
     }
 
     private static class ViewHolder {
